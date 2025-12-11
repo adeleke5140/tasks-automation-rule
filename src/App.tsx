d51bc1e56dff1e32b61bc28303b4bf89eb583eb5
Kenny <adelekekehinde06@gmail.com>
2025-12-11 19:00:10 +0100

    refactor: remove state management for rule type

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Box, Button, Paper, Stack } from '@mantine/core'
 import { Header } from './components/header'
 import { RuleUnitForm } from './components/rule-unit-form'
-import { useState } from 'react'
 
 function App() {
-  const [ruleType, setRuleType] = useState<'valueBased' | 'metricBased'>('valueBased')
   return (
-    <Box p="xl" style={{ maxWidth: 1400, margin: '0 auto' }}>
+    <Box p="xl" style={{ maxWidth: 1144, margin: '0 auto' }}>
       <Stack gap="xl">
         <Header />
 
         <Paper p="lg" bg="m-pink.0" radius="md">
-          <RuleUnitForm ruleType={ruleType} setRuleType={setRuleType} />
+          <RuleUnitForm />
         </Paper>
 
         <Box>
