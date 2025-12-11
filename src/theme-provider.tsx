b0ef62661cf328ddf942e1e9cfc6df1d6783d060
Kenny <adelekekehinde06@gmail.com>
2025-12-12 00:36:56 +0100

    feat: add orange color

--- src/theme-provider.tsx
+++ src/theme-provider.tsx
@@ -5,31 +5,43 @@ import type { ReactNode } from 'react'
 const theme = createTheme({
   fontFamily: 'IBM Plex Sans Variable, sans-serif',
   colors: {
-    "m-pink":[
-  "#fbf7ff",
-  "#e6d1fb",
-  "#cc9df9",
-  "#b067f8",
-  "#993bf7",
-  "#8b22f7",
-  "#8417f8",
-  "#720ddd",
-  "#6508c6",
-  "#5700ad"
-],
-"m-blue": [
-  "#e7f0ff",
-  "#ceddff",
-  "#9bb7ff",
-  "#6490ff",
-  "#386efe",
-  "#1c58fe",
-  "#0b4fff",
-  "#003fe4",
-  "#0038cd",
-  "#002fb5"
-]
-  }
+    'm-pink': [
+      '#fbf7ff',
+      '#e6d1fb',
+      '#cc9df9',
+      '#b067f8',
+      '#993bf7',
+      '#8b22f7',
+      '#8417f8',
+      '#720ddd',
+      '#6508c6',
+      '#5700ad',
+    ],
+    'm-blue': [
+      '#e7f0ff',
+      '#ceddff',
+      '#9bb7ff',
+      '#6490ff',
+      '#386efe',
+      '#1c58fe',
+      '#0b4fff',
+      '#003fe4',
+      '#0038cd',
+      '#002fb5',
+    ],
+    'm-orange': [
+      '#ffece5',
+      '#ffd8cd',
+      '#ffaf9b',
+      '#ff7e5e',
+      '#fe5e36',
+      '#fe4719',
+      '#ff3a09',
+      '#e42c00',
+      '#cb2400',
+      '#b21900',
+    ],
+  },
 })
 
 interface ThemeProviderProps {
