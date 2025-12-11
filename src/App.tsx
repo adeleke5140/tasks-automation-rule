7c35114e0f29af0018e2e85fe05b97950788bda2
Kenny <adelekekehinde06@gmail.com>
2025-12-12 00:37:08 +0100

    feat: add ability to create multiple tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { Box, Button, Stack } from '@mantine/core'
+import { useState } from 'react'
 import { Header } from './components/header'
 import { TaskItem } from './components/task-item'
+import { IconPlus } from '@tabler/icons-react'
+
+interface Task {
+  id: string
+}
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([{ id: '1' }])
+
+  const addTask = () => {
+    setTasks((prev) => [...prev, { id: (prev.length + 1).toString() }])
+  }
+
   return (
     <Box p="xl" style={{ maxWidth: 1144, margin: '0 auto' }}>
       <Stack gap="xl">
         <Header />
 
-        <TaskItem />
+        <Stack gap={'md'}>
+          {tasks.map((task) => (
+            <TaskItem key={task.id} />
+          ))}
+        </Stack>
+
         <Box>
-          <Button variant="subtle" color="gray" leftSection={<span>+</span>}>
+          <Button variant="filled" bg="gray.1" c="black" leftSection={<IconPlus size={16} />} onClick={addTask}>
             Task
           </Button>
         </Box>
