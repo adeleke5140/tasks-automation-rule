96331df13c819d3c32e4f9f093a1c3f00492aa2f
Kenny <adelekekehinde06@gmail.com>
2025-12-11 22:05:37 +0100

    feat: add notification css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './theme-provider.tsx'
 import '@mantine/core/styles.css'
+import '@mantine/notifications/styles.css'
 import '@fontsource-variable/ibm-plex-sans'
 
 createRoot(document.getElementById('root')!).render(
