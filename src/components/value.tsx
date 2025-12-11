f45f664e49435ca626b384438ebe7c6f73170b86
Kenny <adelekekehinde06@gmail.com>
2025-12-11 19:01:34 +0100

    refactor: replace flex with Group

--- src/components/value.tsx
+++ src/components/value.tsx
@@ -1,5 +1,4 @@
-import { ActionIcon, Group, Paper } from '@mantine/core'
-import { IconTrash } from '@tabler/icons-react'
+import { Group, Paper } from '@mantine/core'
 import { useState } from 'react'
 import type { TypeMetric } from '../types/client'
 
@@ -8,42 +7,34 @@ interface ValueProps {
   children: React.ReactNode
 }
 
-const metricsWithCurrency: TypeMetric[] = ['cpm', 'cpc', 'spend', 'purchase_roas', 'current_bid', 'daily_budget']
+const metricsWithCurrency: TypeMetric[] = ['cpm', 'cost', 'spend', 'purchase_roas', 'current_bid', 'daily_budget']
 
 export const Value = ({ selectedMetric, children }: ValueProps) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('0')
   const showCurrencySign = metricsWithCurrency.includes(selectedMetric)
 
   return (
-    <Paper
-      p="md"
-      bg="white"
-      shadow="sm"
-      radius="0"
-      style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: '8px' }}
-    >
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder={showCurrencySign ? '$' : ''}
-        style={{
-          border: 'none',
-          outline: 'none',
-          background: 'transparent',
-          color: 'var(--mantine-color-gray-7)',
-          fontSize: 'var(--mantine-font-size-sm)',
-          padding: 0,
-          flex: 1,
-          minWidth: 0,
-        }}
-      />
-      <Group gap="xs" wrap="nowrap">
-        {children}
-
-        <ActionIcon variant="subtle" color="gray" size="sm">
-          <IconTrash size={18} />
-        </ActionIcon>
+    <Paper p="md" bg="white" shadow="sm" radius="0">
+      <Group align="cneter" justify="space-between" gap={8}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder={showCurrencySign ? '$' : ''}
+          style={{
+            border: 'none',
+            outline: 'none',
+            background: 'transparent',
+            color: 'var(--mantine-color-gray-7)',
+            fontSize: 'var(--mantine-font-size-sm)',
+            padding: 0,
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+        <Group gap="xs" wrap="nowrap">
+          {children}
+        </Group>
       </Group>
     </Paper>
   )
