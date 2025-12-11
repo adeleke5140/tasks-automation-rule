ffde44a8c046663eab103e1feb63fba2cc5435e2
Kenny <adelekekehinde06@gmail.com>
2025-12-11 19:00:48 +0100

    feat: add trash icon to rule selector

--- src/components/rule-selector.tsx
+++ src/components/rule-selector.tsx
@@ -1,5 +1,5 @@
-import { ActionIcon, Menu, Text } from '@mantine/core'
-import { IconCheck, IconSettings } from '@tabler/icons-react'
+import { ActionIcon, Group, Menu, Text } from '@mantine/core'
+import { IconCheck, IconSettings, IconTrash } from '@tabler/icons-react'
 
 interface RuleSelectorProps {
   ruleType: 'valueBased' | 'metricBased'
@@ -8,36 +8,41 @@ interface RuleSelectorProps {
 
 export const RuleSelector = ({ ruleType, setRuleType }: RuleSelectorProps) => {
   return (
-    <Menu position="bottom-end" shadow="md">
-      <Menu.Target>
-        <ActionIcon variant="subtle" color="gray" size="sm">
-          <IconSettings size={18} />
-        </ActionIcon>
-      </Menu.Target>
+    <Group>
+      <Menu position="bottom-end" shadow="md">
+        <Menu.Target>
+          <ActionIcon variant="subtle" color="gray" size="sm">
+            <IconSettings size={18} />
+          </ActionIcon>
+        </Menu.Target>
 
-      <Menu.Dropdown>
-        <Menu.Label>Compare with</Menu.Label>
-        <Menu.Item
-          onClick={() => setRuleType('valueBased')}
-          rightSection={
-            ruleType === 'valueBased' ? <IconCheck size={16} color="var(--mantine-color-m-blue-6)" /> : null
-          }
-        >
-          <Text size="sm" fw={500} c={ruleType === 'valueBased' ? 'm-blue.6' : 'gray.7'}>
-            Value
-          </Text>
-        </Menu.Item>
-        <Menu.Item
-          onClick={() => setRuleType('metricBased')}
-          rightSection={
-            ruleType === 'metricBased' ? <IconCheck size={16} color="var(--mantine-color-m-blue-6)" /> : null
-          }
-        >
-          <Text size="sm" fw={500} c={ruleType === 'metricBased' ? 'm-blue.6' : 'gray.7'}>
-            Metric
-          </Text>
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+        <Menu.Dropdown>
+          <Menu.Label>Compare with</Menu.Label>
+          <Menu.Item
+            onClick={() => setRuleType('valueBased')}
+            rightSection={
+              ruleType === 'valueBased' ? <IconCheck size={16} color="var(--mantine-color-m-blue-6)" /> : null
+            }
+          >
+            <Text size="sm" fw={500} c={ruleType === 'valueBased' ? 'm-blue.6' : 'gray.7'}>
+              Value
+            </Text>
+          </Menu.Item>
+          <Menu.Item
+            onClick={() => setRuleType('metricBased')}
+            rightSection={
+              ruleType === 'metricBased' ? <IconCheck size={16} color="var(--mantine-color-m-blue-6)" /> : null
+            }
+          >
+            <Text size="sm" fw={500} c={ruleType === 'metricBased' ? 'm-blue.6' : 'gray.7'}>
+              Metric
+            </Text>
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+      <ActionIcon variant="subtle" color="gray" size="sm">
+        <IconTrash size={18} />
+      </ActionIcon>
+    </Group>
   )
 }
