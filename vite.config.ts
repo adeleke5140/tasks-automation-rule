774e820ba763b112283146d8505dcf0647aaa400
Kenny <adelekekehinde06@gmail.com>
2025-12-13 14:25:53 +0100

    feat: install and configure sass preprocessor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "${path.join(process.cwd(), 'src/_mantine').replace(/\\/g, '/')}" as mantine;`,
+      },
+    },
+  },
 })
