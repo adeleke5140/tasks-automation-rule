b9a1b2fd0970cf4287fe9a5d1de6f0ad2b5b93da
Kenny <adelekekehinde06@gmail.com>
2025-12-14 11:50:28 +0100

    feat: pass in required prop

--- src/components/rule/value-rule-form.tsx
+++ src/components/rule/value-rule-form.tsx
@@ -9,18 +9,34 @@ import { Value } from '../value'
 import type { RuleTypeFormProps } from './rule-unit-form'
 import { Range } from '../range'
 
-export const ValueRuleForm = ({ id, ruleType, payload, onDelete, onChangeRuleType }: RuleTypeFormProps<'valueBased'>) => {
+export const ValueRuleForm = ({
+  id,
+  ruleType,
+  payload,
+  onDelete,
+  onChangeRuleType,
+  isSelectable,
+  isSelected,
+  onToggleSelection
+}: RuleTypeFormProps<'valueBased'> & { isSelectable?: boolean; isSelected?: boolean; onToggleSelection?: () => void }) => {
   const [selectedMetric, setSelectedMetric] = useState<TypeMetric>(payload?.metric || 'cost')
   const [selectedRange, setSelectedRange] = useState<TypeRange>(payload?.range || 'today')
   const [selectedOperator, setSelectedOperator] = useState(payload?.operator || 'lt')
   const [selectedValue, setSelectedValue] = useState(payload?.value || 0)
+
   return (
     <Group pos="relative" gap={0} wrap="nowrap" align="stretch">
       <Connector />
 
       <Stack gap="lg" style={{ flex: 1 }}>
         <Group gap={0} wrap="nowrap">
-          <Metric selectedMetric={selectedMetric} onMetricChange={setSelectedMetric} />
+          <Metric
+            selectedMetric={selectedMetric}
+            onMetricChange={setSelectedMetric}
+            isSelectable={isSelectable}
+            isSelected={isSelected}
+            onToggleSelection={onToggleSelection}
+          />
           <Range selectedRange={selectedRange} onRangeChange={setSelectedRange} />
           <Operator selectedOperator={selectedOperator} onOperatorChange={setSelectedOperator} />
           <Value selectedMetric={selectedMetric} selectedValue={selectedValue} onValueChange={setSelectedValue}>
