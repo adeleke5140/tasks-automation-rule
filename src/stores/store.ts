c5af0acd19a493695eaa00c237fe9ccd44ed2edc
Kenny <adelekekehinde06@gmail.com>
2025-12-14 13:40:51 +0100

    feat: add new task reducer

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+import tasksReducer from '@/store/slices/tasksSlice'
 import conditionsReducer from '@/store/slices/conditionsSlice'
-import selectionReducer from '@/store/slices/selectionSlice'
 import actionReducer from '@/store/slices/actionSlice'
+import selectionReducer from '@/store/slices/selectionSlice'
 
 export const store = configureStore({
   reducer: {
+    tasks: tasksReducer,
     conditions: conditionsReducer,
-    selection: selectionReducer,
     action: actionReducer,
+    selection: selectionReducer,
   },
 })
 
