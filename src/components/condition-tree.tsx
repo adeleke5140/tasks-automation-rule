c011ded9ad8ea33ffd4977059b8b77038c510f69
Kenny <adelekekehinde06@gmail.com>
2025-12-14 02:17:25 +0100

    feat: use rtk stores

--- src/components/condition-tree.tsx
+++ src/components/condition-tree.tsx
@@ -1,28 +1,58 @@
 import { SimpleGrid, Stack } from '@mantine/core'
+import { notifications } from '@mantine/notifications'
 import { RelationConnector } from './relation-connector'
 import { RuleUnitForm } from './rule/rule-unit-form'
 import type { Condition } from './task-item'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { deleteCondition } from '../store/slices/conditionsSlice'
+import { toggleSelection, isIdSelected } from '../store/slices/selectionSlice'
 
 type ConditionTreeProps = {
   cond: Condition
-  selectionIds: Set<string>
-  onToggleSelection: (item: string) => void
-  isSelectable: boolean
 }
 
-export const ConditionTree = ({ cond, selectionIds, onToggleSelection, isSelectable }: ConditionTreeProps) => {
+export const ConditionTree = ({ cond }: ConditionTreeProps) => {
+  const dispatch = useAppDispatch()
+  const isSelected = useAppSelector(state => isIdSelected(state, cond.id))
+  const totalConditions = useAppSelector(state => {
+    let count = 0
+    const countConditions = (items: Array<Condition>) => {
+      items.forEach(item => {
+        if (item.type === 'condition') count++
+        else countConditions(item.children)
+      })
+    }
+    countConditions(state.conditions.conditions)
+    return count
+  })
+
   if (cond.type === 'condition') {
-    const deleteCondition = (id: string) => {
-      console.log('deleted', id)
+    const handleDelete = (id: string) => {
+      if (totalConditions <= 1) {
+        notifications.show({
+          title: 'Cannot delete',
+          message: 'At least one condition must remain',
+          color: 'red'
+        })
+        return
+      }
+      dispatch(deleteCondition(id))
+    }
+
+    const handleToggleSelection = () => {
+      dispatch(toggleSelection(cond.id))
     }
 
     return (
       <RuleUnitForm
         id={cond.id}
         indicatorColor=""
-        onDelete={(id) => deleteCondition(id)}
+        onDelete={handleDelete}
         ruleType={cond.ruleType}
         payload={cond.payload[cond.ruleType]}
+        isSelectable={true}
+        isSelected={isSelected}
+        onToggleSelection={handleToggleSelection}
       />
     )
   }
@@ -38,16 +68,8 @@ export const ConditionTree = ({ cond, selectionIds, onToggleSelection, isSelecta
     >
       <RelationConnector connectionType={cond.relation} />
       <Stack component={'div'} gap="md" style={{ flex: 1, marginLeft: '25px' }} data-relation="true">
-        {cond.children.map((item, i) => {
-          return (
-            <ConditionTree
-              key={i}
-              cond={item}
-              selectionIds={selectionIds}
-              onToggleSelection={onToggleSelection}
-              isSelectable={isSelectable}
-            />
-          )
+        {cond.children.map((item) => {
+          return <ConditionTree key={item.id} cond={item} />
         })}
       </Stack>
     </SimpleGrid>
