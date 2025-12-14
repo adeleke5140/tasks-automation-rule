0f53d9d4691cb5d73d3fd86689a33c66e1a595cf
Kenny <adelekekehinde06@gmail.com>
2025-12-14 02:39:50 +0100

    feat:add more props for nested components

--- src/components/rule/rule-unit-form.tsx
+++ src/components/rule/rule-unit-form.tsx
@@ -3,8 +3,11 @@ import type { Payload, RuleType, RuleValues } from './types'
 import { ValueRuleForm } from './value-rule-form'
 
 export type RuleTypeFormProps<T extends RuleType> = {
+  id: string
   ruleType: T
   payload: RuleValues[T]['payload']
+  onDelete: (id: string) => void
+  onChangeRuleType: (ruleType: RuleType) => void
 }
 
 export type RuleUnitFormProps<T extends RuleType = RuleType> = {
@@ -13,11 +16,24 @@ export type RuleUnitFormProps<T extends RuleType = RuleType> = {
   onDelete: (id: string) => void
   ruleType: T
   payload: Payload[T]
+  isSelectable?: boolean
+  isSelected?: boolean
+  onToggleSelection?: () => void
 }
 
 export const RuleUnitForm = (props: RuleUnitFormProps) => {
-  if (props.ruleType === 'metricBased') {
-    return <MetricRuleForm {...(props as RuleUnitFormProps<'metricBased'>)} />
+  const formProps = {
+    id: props.id,
+    ruleType: props.ruleType,
+    payload: props.payload,
+    onDelete: props.onDelete,
+    onChangeRuleType: (newRuleType: RuleType) => {
+      console.log('Change rule type to:', newRuleType)
+    }
   }
-  return <ValueRuleForm {...(props as RuleTypeFormProps<'valueBased'>)} />
+
+  if (props.ruleType === 'metricBased') {
+    return <MetricRuleForm {...(formProps as RuleTypeFormProps<'metricBased'>)} />
+  }
+  return <ValueRuleForm {...(formProps as RuleTypeFormProps<'valueBased'>)} />
 }
