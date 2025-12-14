504f4a6e0deb6d211d25558d2fe072b87e62efc0
Kenny <adelekekehinde06@gmail.com>
2025-12-14 12:51:26 +0100

    chore: update import

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import conditionsReducer from '../store/slices/conditionsSlice'
-import selectionReducer from '../store/slices/selectionSlice'
-import actionReducer from '../store/slices/actionSlice'
+import conditionsReducer from '@/store/slices/conditionsSlice'
+import selectionReducer from '@/store/slices/selectionSlice'
+import actionReducer from '@/store/slices/actionSlice'
 
 export const store = configureStore({
   reducer: {
