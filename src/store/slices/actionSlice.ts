96da674e0ccae53267f821338e1e32d563014f84
Kenny <adelekekehinde06@gmail.com>
2025-12-14 02:19:55 +0100

    feat: create action slice

---
+++ src/store/slices/actionSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+import type { TypeTask } from '../../types/client'
+
+type Action = TypeTask['action']
+type ObjectType = TypeTask['objectType']
+
+interface ActionState {
+  selectedAction: Action
+  objectType: ObjectType
+}
+
+const initialState: ActionState = {
+  selectedAction: 'pause',
+  objectType: 'adset', // Default to adset
+}
+
+const actionSlice = createSlice({
+  name: 'action',
+  initialState,
+  reducers: {
+    setSelectedAction: (state, action: PayloadAction<Action>) => {
+      state.selectedAction = action.payload
+    },
+    setObjectType: (state, action: PayloadAction<ObjectType>) => {
+      state.objectType = action.payload
+    },
+  },
+})
+
+export const { setSelectedAction, setObjectType } = actionSlice.actions
+
+export const selectAction = (state: { action: ActionState }) => state.action.selectedAction
+export const selectObjectType = (state: { action: ActionState }) => state.action.objectType
+
+export const getObjectTypeDisplay = (objectType: ObjectType): string => {
+  const map: Record<ObjectType, string> = {
+    campaign: 'Campaigns',
+    adset: 'Adsets',
+    ad: 'Ads',
+  }
+  return map[objectType] || objectType
+}
+
+export default actionSlice.reducer
