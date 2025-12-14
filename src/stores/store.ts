4b8ae8620ff896bca0136029524ae2a4f37c9108
Kenny <adelekekehinde06@gmail.com>
2025-12-14 02:40:32 +0100

    feat: add reduces to store

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
+import conditionsReducer from '../store/slices/conditionsSlice'
+import selectionReducer from '../store/slices/selectionSlice'
+import actionReducer from '../store/slices/actionSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    conditions: conditionsReducer,
+    selection: selectionReducer,
+    action: actionReducer,
+  },
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
